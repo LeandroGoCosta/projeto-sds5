40f698cb84c33991d55adde267d30cd6d1d7e255
LeandroGoCosta <leandro.gocosta@gmail.com>
2021-11-05 09:33:50 -0300

    primeito commit

---
+++ backend/src/main/java/com/inuvens/ivendas/IvendasApplication.java
@@ -0,0 +1,13 @@
+package com.inuvens.ivendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/inuvens/ivendas/IvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inuvens.ivendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
